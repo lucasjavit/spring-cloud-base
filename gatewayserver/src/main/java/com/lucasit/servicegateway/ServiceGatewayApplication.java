7ff0ad174db3837d23b420394fa532b6bb5ab37d
vieiralucas89 <vyeiralucas@gmail.com>
2022-10-25 21:55:32 -0300

    first commit

---
+++ gatewayserver/src/main/java/com/lucasit/servicegateway/ServiceGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.lucasit.servicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+
+}
---
+++ gatewayserver/src/test/java/com/lucasit/servicegateway/ServiceGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasit.servicegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
